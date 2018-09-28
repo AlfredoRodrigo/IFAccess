7627de40512353f9159c0dab9ea8a9a3d45fb867
Alfredo Silva <alf_rodrigo14@hotmail.com>
2018-09-28 02:28:01 -0300

    Implementando a adição de pessoas, ambientes e horários usando forms do Django, e melhorando o CSS.

---
+++ ifaccapp/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import Person
+from .models import Ambient
+from .models import Schedule
+
+class PersonForm(forms.ModelForm):
+    class Meta:
+        model = Person
+        fields = ('name', 'registration', 'tag')
+
+class AmbientForm(forms.ModelForm):
+    class Meta:
+        model = Ambient
+        fields = ('name', 'ID')
+
+class ScheduleForm(forms.ModelForm):
+    class Meta:
+        model = Schedule
+        fields = ('day', 'entryTime', 'exitTime', 'person', 'ambient')
--- ifaccapp/views.py
+++ ifaccapp/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from .models import Ambient
 from .models import Person
 from .models import Schedule
+from .forms import PersonForm
+from .forms import AmbientForm
+from .forms import ScheduleForm
 
 # Create your views here.
 
@@ -22,10 +26,34 @@ def schedules(request):
     return render(request, 'ifaccapp/schedules.html', {'schedules': schedules})
 
 def register_ambient(request):
+    if request.method == "POST":
+        register_ambient = AmbientForm(request.POST)
+        if register_ambient.is_valid():
+            ambient = register_ambient.save()
+            ambient.save()
+            return redirect('ambients')
+    else:
+        register_ambient = AmbientForm()
     return render(request, 'ifaccapp/register/register_ambient.html', {'register_ambient': register_ambient})
 
 def register_person(request):
+    if request.method == "POST":
+        register_person = PersonForm(request.POST)
+        if register_person.is_valid():
+            person = register_person.save()
+            person.save()
+            return redirect('people')
+    else:
+        register_person = PersonForm()
     return render(request, 'ifaccapp/register/register_person.html', {'register_person': register_person})
 
 def register_schedule(request):
+    if request.method == "POST":
+        register_schedule = ScheduleForm(request.POST)
+        if register_schedule.is_valid():
+            schedule = register_schedule.save()
+            schedule.save()
+            return redirect('schedules')
+    else:
+        register_schedule = ScheduleForm()
     return render(request, 'ifaccapp/register/register_schedule.html', {'register_schedule': register_schedule})
