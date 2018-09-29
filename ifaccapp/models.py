fdef3ba6fae2410e982f3fed648ac65105996a1a
Alfredo Silva <alf_rodrigo14@hotmail.com>
2018-09-28 21:34:58 -0300

    Melhorando a interface das telas principais.

--- ifaccapp/models.py
+++ ifaccapp/models.py
@@ -1,16 +1,15 @@
 from django.db import models
 
-
 class Person(models.Model):
-    name = models.TextField(null=True)
-    registration = models.BigIntegerField(primary_key=True)  # matrícula
+    name = models.TextField(null=True, max_length=100, verbose_name="Nome")
+    registration = models.BigIntegerField(primary_key=True, verbose_name="Matrícula")  # matrícula
     tag = models.IntegerField(null=True)
 
     def __str__(self):
         return self.name
 
 class Ambient(models.Model):
-    name = models.TextField(null=True)
+    name = models.TextField(null=True, max_length=50, verbose_name="Nome")
     ID = models.TextField(primary_key=True)
 
     def __str__(self):
@@ -18,8 +17,26 @@ class Ambient(models.Model):
 
 
 class Schedule(models.Model):
-    day = models.DateField(null=True)
-    entryTime = models.TimeField(null=True)
-    exitTime = models.TimeField(null=True)
-    person = models.ForeignKey('Person', on_delete=models.CASCADE)
-    ambient = models.ForeignKey('Ambient', on_delete=models.CASCADE)
+    DOMINGO = 'Domingo'
+    SEGUNDA = 'Segunda-feira'
+    TERCA = 'Terça-feira'
+    QUARTA = 'Quarta-feira'
+    QUINTA = 'Quinta-feira'
+    SEXTA = 'Sexta-feira'
+    SABADO = 'Sábado'
+
+    DAY_CHOICES = (
+        (DOMINGO, 'Domingo'),
+        (SEGUNDA, 'Segunda-feira'),
+        (TERCA, 'Terça-feira'),
+        (QUARTA, 'Quarta-feira'),
+        (QUINTA, 'Quinta-feira'),
+        (SEXTA, 'Sexta-feira'),
+        (SABADO, 'Sábado'),
+    )
+
+    day = models.TextField(null=True, max_length=50, choices=DAY_CHOICES, verbose_name="Dia")
+    entryTime = models.TimeField(null=True, verbose_name="Hora de entrada")
+    exitTime = models.TimeField(null=True, verbose_name="Hora de saída")
+    person = models.ForeignKey('Person', on_delete=models.CASCADE, verbose_name="Pessoa")
+    ambient = models.ForeignKey('Ambient', on_delete=models.CASCADE, verbose_name="Ambiente")
--- ifaccess/urls.py
+++ ifaccess/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # daqui para baixo, deveria se utilizar o include, e todas essas linhas deveriam estar no arquivo ifaccapp/urls.py
     path('', views.home, name='home'),
+    path('home.html', views.home, name='home'),
     path('people.html', views.people, name='people'),
     path('ambients.html', views.ambients, name='ambients'),
     path('schedules.html', views.schedules, name='schedules'),
