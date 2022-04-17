6b8ce79d4ce250c5a4b89b5f74403bff58fecf9a
AlfredoRodrigo <alfredorodrigo60@gmail.com>
2022-04-17 19:53:09 -0300

    Adicionando signup e parte do login

--- ifaccapp/forms.py
+++ ifaccapp/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from .models import Person
 from .models import Ambient
 from .models import Schedule
+from .models import Signup
+from .models import Login
 
 class PersonForm(forms.ModelForm):
     class Meta:
@@ -17,3 +19,20 @@ class ScheduleForm(forms.ModelForm):
     class Meta:
         model = Schedule
         fields = ('day', 'entryTime', 'exitTime', 'person', 'ambient')
+
+class SignupForm(forms.ModelForm):
+    class Meta:
+        model = Signup
+        fields = ('user', 'password', 'confirmPassword')
+        widgets = {
+            'password': forms.PasswordInput(),
+            'confirmPassword': forms.PasswordInput(),
+        }
+
+class LoginForm(forms.ModelForm):
+    class Meta:
+        model = Login
+        fields = ('user', 'password')
+        widgets = {
+            'password': forms.PasswordInput(),
+        }
--- ifaccess/settings.py
+++ ifaccess/settings.py
@@ -121,3 +121,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+# Personal modifications (inserted by Alfredo Silva)
+
+LOGIN_URL = '/login/'
